e0ab83fc30dcde1df9e610d61b3730b54871f14d
leahabiol <leah.abiol@protonmail.com>
2017-01-19 13:13:42 -0800

    Update hello.py
    Change of animal.

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 state = "California"
-animal = "frog"
+animal = "ladybug"
 name = "Lee Attewode"
 number = 10
 print "In %s there are countless %ss, but %s has counted %d!" % (state, animal, name, number)
